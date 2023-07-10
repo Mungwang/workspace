8a9dee327ad585f9e842f8598b6d027ab928316e
GWANG <mungwang1208@gmail.com>
2023-07-10 12:33:07 +0900

    07-10 오전

--- 5_Server/community 사용자 계정.sql
+++ 5_Server/community 사용자 계정.sql
@@ -33,3 +33,18 @@ VALUES(SEQ_MEMBER_NO.NEXTVAL, 'user01@kh.or.kr', 'pass01!',
 
 COMMIT;
 
+
+--암호화된 비밀번호로 업데이트
+UPDATE MEMBER SET
+MEMBER_PW = 'aBN5hiegXlvAovJLipPoPd5LB+xjPrAeu1tcAVg0p5MKGocvo6l825SD+ZMCOcHBFeGB7MnzH31SAnDzYYsSdg==';
+
+-- MEMBER TABLE 비밀번호 컬럼 크기 변경
+ALTER TABLE MEMBER
+MODIFY MEMBER_PW VARCHAR2(100);
+
+-- 암호화된 비밀번호로 업데이트
+UPDATE MEMBER SET
+MEMBER_PW = 'aBN5hiegXlvAovJLipPoPd5LB+xjPrAeu1tcAVg0p5MKGocvo6l825SD+ZMCOcHBFeGB7MnzH31SAnDzYYsSdg=='
+WHERE MEMBER_NO ='1';
+
+commit;
--- 5_Server/community/src/main/java/edu/kh/community/common/filter/EncodingFilter.java
+++ 5_Server/community/src/main/java/edu/kh/community/common/filter/EncodingFilter.java
@@ -4,43 +4,44 @@ import java.io.IOException;
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
 import javax.servlet.FilterConfig;
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.annotation.WebFilter;
 import javax.servlet.http.HttpFilter;
+import javax.servlet.http.HttpServletRequest;
 
-/* 필터(Filter)
- * 
- * - 클라이언트 요청 시 생성되는
- *   HttpServletRequest, HttpServletResponse가
- *   요청을 처리하는 Servlet에 도달하기 전에
- *   특정 코드를 수행하는 클래스
- *   
- *   [요청 흐름]
- *   클라이언트 -> 요청 -> HttpServletRequest -> 필터 -> 요청 처리 Servlet
- *   				  HttpServletResponse
- *   					생성
- * 
- * - 여러 필터를 만들어 연쇄적으로 연결하여 수행할 수 있다. (FilterChain)
- * 
- * */
-
-
-// @WebFilter("url패턴")
-// - 해당 클래스를 필터 클래스로 등록
-// - url패턴에 일치하는 요청이 있을 경우 헤당 요청을 필터링함
-
-// filterName 속성 : 필터의 이름 지정, 필터 순서 자정 시 사용
-// urlPatterns 속성 : 요청 주소 패턴
-
-// / == /community == 최상위 주소
-// * == 모든
-// /* == 최상위 주소 하위 모든 == 모든 요청
+		/* 필터(Filter)
+		 * 
+		 * - 클라이언트 요청 시 생성되는
+		 *   HttpServletRequest, HttpServletResponse가
+		 *   요청을 처리하는 Servlet에 도달하기 전에
+		 *   특정 코드를 수행하는 클래스
+		 *   
+		 *   [요청 흐름]
+		 *   클라이언트 -> 요청 -> HttpServletRequest -> 필터 -> 요청 처리 Servlet
+		 *   				  HttpServletResponse
+		 *   					생성
+		 * 
+		 * - 여러 필터를 만들어 연쇄적으로 연결하여 수행할 수 있다. (FilterChain)
+		 * 
+		 * */
+		
+		
+		// @WebFilter("url패턴")
+		// - 해당 클래스를 필터 클래스로 등록
+		// - url패턴에 일치하는 요청이 있을 경우 헤당 요청을 필터링함
+		
+		// filterName 속성 : 필터의 이름 지정, 필터 순서 자정 시 사용
+		// urlPatterns 속성 : 요청 주소 패턴
+		
+		// / == /community == 최상위 주소
+		// * == 모든
+		// /* == 최상위 주소 하위 모든 == 모든 요청
 
 
 @WebFilter(filterName = "encodingFilter", urlPatterns="/*")
-// @@@@@@@@@@@@@@@@@@@@
 public class EncodingFilter extends HttpFilter implements Filter {
 	
 	
@@ -70,6 +71,18 @@ public class EncodingFilter extends HttpFilter implements Filter {
 		
 		// 모든 응답의 문자 인코딩을 UTF-8로 설정
 		response.setCharacterEncoding("UTF-8");
+		
+		// application scope로 최상위 경로를 얻어올 수 있는 값세팅
+		
+		// application 내장 객체 얻어오기
+		ServletContext application = request.getServletContext();
+		
+		// 최상위 주소 얻어오기
+		String contextPath = ((HttpServletRequest)request).getContextPath();
+							 // 다운캐스팅
+		
+		// 세팅
+		application.setAttribute("contextPath", contextPath);
 		
 		// 연결된 다음 필터를 수행(없으면 Servlet 수행)
 		chain.doFilter(request, response);
