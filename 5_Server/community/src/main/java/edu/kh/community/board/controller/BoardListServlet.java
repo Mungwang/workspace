95347d05fec36c9f962457c21593bccef9ac5055
GWANG <mungwang1208@gmail.com>
2023-07-17 16:01:44 +0900

    07-17 end

--- 5_Server/community/src/main/java/edu/kh/community/board/controller/BoardListServlet.java
+++ 5_Server/community/src/main/java/edu/kh/community/board/controller/BoardListServlet.java
@@ -1,6 +1,7 @@
 package edu.kh.community.board.controller;
 
 import java.io.IOException;
+import java.util.Map;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -21,28 +22,24 @@ public class BoardListServlet extends HttpServlet {
 			
 			// 쿼리스트링 얻어오기 == 파라미터 얻어오기
 			
-			// /board/list?type=1
 			int type = Integer.parseInt(req.getParameter("type"));
-			
-			System.out.println("type : " + type);			
-			
-			// /board/list?type=1$cp=2
 			
 			// nav 메뉴(공지사항,자유게시판, 질문게시판) 선택시
 			// 쿼리스트링에 cp가 없음 --> cp = 1 고정
 			
 			int cp =1;
 			
+			// 페이지네이션 번호 선택 시
+			// 쿼리스트링에 cp가 있음 --> cp = 쿼리스트링의 cp값
+			
 			if(req.getParameter("cp")!=null) { // 쿼리스트링에 "cp"가 존재한다면
-				
 				cp = Integer.parseInt(req.getParameter("cp"));
 			}
-		
-			
-			System.out.println("cp : " + cp);
-			
 			
 			BoardService service = new BoardService();
+			
+			// 게시판 이름, 페이지네이션 객체, 게시글 리스트 한번에 반화하는 Service 호출
+			Map<String,Object> map = service.selectBoardList(type, cp);
 			
 			String path = "/WEB-INF/views/board/boardList.jsp";
 			
--- 5_Server/community/src/main/java/edu/kh/community/board/model/dao/BoardDAO.java
+++ 5_Server/community/src/main/java/edu/kh/community/board/model/dao/BoardDAO.java
@@ -3,10 +3,15 @@ package edu.kh.community.board.model.dao;
 import static edu.kh.community.common.JDBCTemplate.*;
 
 import java.io.FileInputStream;
+import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Properties;
+
+import edu.kh.community.member.model.vo.Member;
 
 public class BoardDAO {
    
@@ -27,5 +32,35 @@ public class BoardDAO {
          e.printStackTrace();
       }
    }
+
+	/** 게시글 이름 조회 DAO
+	 * @param conn
+	 * @param type
+	 * @return
+	 * @throws Exception
+	 */
+	public String selectBoardName(Connection conn, int type) throws Exception {
+		String boardName = null;
+		
+		try {
+			String sql = prop.getProperty("selectBoardName");
+			
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setInt(1, type);
+			
+			rs = pstmt.executeQuery();
+			
+			if(rs.next()) {
+				
+				boardName = rs.getString(1);
+			}
+			
+		} finally {
+			close(rs);
+			close(stmt);
+		}
+		
+		return boardName;
+	}
 
 }
