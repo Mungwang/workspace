b290314dbc41e50d46c6058b11212f7c6828b35a
Gwang <mungwang1208@gmail.com>
2023-08-14 17:25:28 +0900

    08-14 End

--- 6_Framework/boardProject/src/main/java/edu/kh/project/myPage/controller/MyPageController.java
+++ 6_Framework/boardProject/src/main/java/edu/kh/project/myPage/controller/MyPageController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.SessionAttribute;
 import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import edu.kh.project.member.model.dto.Member;
 import edu.kh.project.myPage.model.service.MyPageService;
@@ -52,7 +53,8 @@ public class MyPageController {
 	// 회원 정보 수정
 	@PostMapping("/info")
 	public String info(Member updateMember,String[] memberAddress
-					   ,@SessionAttribute("loginMember") Member loginMember) {
+					   ,@SessionAttribute("loginMember") Member loginMember
+					   , RedirectAttributes ra ) {
 		
 		// ------------------------매개 변수 설명 ------------------------
 		// Member updateMember : 수정할 닉네임, 전화번호 담긴 커맨드 객체
@@ -62,6 +64,8 @@ public class MyPageController {
 		// : Session에서 얻어옴 "loginMember"에 해당하는 객체를
 		// : 매개변수 Member loginMember에 저장
 		
+		// RedirectAttributes ra : 리다이렉트 시 값 전달용 객체 (request)
+		// -----------------------------------------------------------
 		// 주소 하나로 합치기(a^^^b^^^c^^^)
 		String addr = String.join("^^^", memberAddress);
 		updateMember.setMemberAddress(addr);
@@ -69,7 +73,28 @@ public class MyPageController {
 		// 로그인한 회원의 번호를 updateMember에 추가
 		updateMember.setMemberNo(loginMember.getMemberNo());
 		
-		return null;
+		// DB에 회원 정보 수정(UPDATE) 서비스 호출
+		int result = service.updateInfo(updateMember);
+		
+		String message = null;
+		
+		if(result >0 ) { // 성공
+			
+			message = "회원 정보가 수정되었습니다.";
+			
+			// Session에 로그인한 회원 정보도 수정(동기화)
+			loginMember.setMemberNickname(updateMember.getMemberNickname());
+			loginMember.setMemberTel(updateMember.getMemberTel());
+			loginMember.setMemberAddress(updateMember.getMemberAddress());
+			
+		} else { // 실패
+			
+			message = "회원 정보 수정 실패.";
+		}
+		
+		ra.addFlashAttribute("message", message);
+		
+		return "redirect:info"; // 상대경로 (/myPage/info GET방식)
 		
 	}
 	
