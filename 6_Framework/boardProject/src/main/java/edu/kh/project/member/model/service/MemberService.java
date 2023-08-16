9dfc657b53d09cd587ffdc9fdcd5c1c2289c64a1
Gwang <mungwang1208@gmail.com>
2023-08-16 17:51:06 +0900

    08-16 End

--- 6_Framework/boardProject/src/main/java/edu/kh/project/member/model/service/MemberService.java
+++ 6_Framework/boardProject/src/main/java/edu/kh/project/member/model/service/MemberService.java
@@ -8,7 +8,7 @@ import edu.kh.project.member.model.dto.Member;
 
 // 2. 클래스간의 결합도를 약화 시키기 위해서(객체 지향적 설계) * 쉽게말해서 리모컨안에 건전지 같은 [ 공통 메소드를 이용하여 프로젝트를 수행한 작업물에 능률이 좋은거로 바꾸기 용이함] * 
 
-// 3. Spring AOP 사용을 위해서
+// 3. Spring AOP 사용을 위해서 ..
 
 
 public interface MemberService {
@@ -24,5 +24,5 @@ public interface MemberService {
 	 * @return result(0:실패, 1:성공)
 	 */
 	int signUp(Member inputMember);
-
+	
 }
