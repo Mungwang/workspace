4e3bb7c5a879cbcc17c07335b9ec030f4f0b0830
Gwang <mungwang1208@gmail.com>
2023-08-21 17:31:19 +0900

    08-21 END

--- 6_Framework/boardProject/src/main/java/edu/kh/project/board/controller/BoardController.java
+++ 6_Framework/boardProject/src/main/java/edu/kh/project/board/controller/BoardController.java
@@ -1,10 +1,14 @@
 package edu.kh.project.board.controller;
 
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.SessionAttributes;
 
 import edu.kh.project.board.model.service.BoardService;
@@ -30,13 +34,43 @@ public class BoardController {
 	
 	// @PathVariable
 	// URL 경로에 있는 값을 매개변수로 이용할 수있게 하는 어노테이션
+	// * + request scope에 세팅* 
+	
+	// @PathVariable 사용하는 경우
+	// - 자원(resource) 구분 / 식별
+	// ex) http://github.com/Hyeseon26
+	// ex) http://github.com/testUser
+	// ex) /board?1 -> 1번(공지사항) 게시판
+	// ex) /board?2 -> 2번(자유게시판) 게시판
+		
+	// Query String을 사용하는 경우
+	// - 정렬, 필터링
+	// ex) search.naver.com?query=날씨
+	// ex) search.naver.com?query=점심
+	// ex) /board?/insert?code=1
+	// ex) /board?/insert?code=2
+	// -> 삽입이라는 공통된 동작 수행
+	//	  단, code에 따라 어디에 삽입할지 지정(필터링)
+	
+	// ex) /board/list?order=recent (최신순)
+	// ex) /board/list?order=most (인기순)
 	
 	// 게시글 목록 조회
 	@GetMapping("/{boardCode}")
-	public String selectBoardList(@PathVariable("boardCode") int boardCode) {
+	public String selectBoardList(@PathVariable("boardCode") int boardCode
+	, @RequestParam(value="cp", required = false, defaultValue = "1") int cp
+	, Model model) {
 		
 		// boardCode 확인
-		System.out.println("boardCode : " +boardCode);
+		//System.out.println("boardCode : " +boardCode);
+		
+		// 게시글 목록 조회 서비스 호출
+		Map<String, Object> map = service.selectBoardList(boardCode,cp);
+		
+		// 조회 결과를 request scope에 세팅 후 forward
+		
+		model.addAttribute("map",map);
+		
 		return "board/boardList";
 		
 	}
