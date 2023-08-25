08155c10922c2f7257be5e2ae99dff310248429a
Gwang <mungwang1208@gmail.com>
2023-08-25 17:48:42 +0900

    08-25

--- 6_Framework/openDataProject/src/main/java/com/kh/opendata/controller/OpenAPIController.java
+++ 6_Framework/openDataProject/src/main/java/com/kh/opendata/controller/OpenAPIController.java
@@ -3,24 +3,25 @@ package com.kh.opendata.controller;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.net.URLEncoder;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class OpenAPIController {
 
-	// 발급받은 인증키 변수처리
+	// 발급받은 인증키 변수처리			    		 
 	public static final String SERVICEKEY = "hLV7ZBzor8MGumIhI2cTG1%2FDjIuhT%2B6ujKm09Uqk%2Bk9qY0b5P1oH2C6P2Otth3yZz7rH9fA7Gi9ZgsiXFMfAxQ%3D%3D";
 
 
 	// Json형식으로 대기오염 OpenAPI 활용하기
 	@ResponseBody
-	@GetMapping(value="air", produces = "application/json; charset=UTF-8")
+	@RequestMapping(value="air", produces = "application/json; charset=UTF-8")
 	public String airMethod(String location) throws IOException {
 
 		// OpenAPI 서버로 요청하고자 하는 url 작성
@@ -29,31 +30,130 @@ public class OpenAPIController {
 		url += "&sidoName=" + URLEncoder.encode(location,"UTF-8"); // 지역명 추가(한글이 들어가면 인코딩 처리해야함)
 		url += "&returnType=json"; // 리턴 타입
 		url += "&numOfRows=20"; // 결과 개수
-		
+
 		// 1. 작성된 url 정보를 넣어 URL 객체 생성
 		URL requestUrl = new URL(url);
-		
+
 		// 2. 생성된 URL 객체로 URLConnection 생성
 		HttpURLConnection urlConn = (HttpURLConnection) requestUrl.openConnection();
-		
+
 		// 3. 요청 시 필요한 Header 설정
 		urlConn.setRequestMethod("GET");
-		
+
 		// 4. 해당 OpenAPI 서버로 요청 후 입력스트림을 통해서 응답데이터 읽어오기
 		BufferedReader br = new BufferedReader(new InputStreamReader(urlConn.getInputStream()));
-		
+
 		String responseText = "";
 		String line;
-		
+
 		while((line = br.readLine()) != null){
 			responseText += line;
 		}
-		
+
 		// 5. 다 사용한 스트링 반납 및 연결 해제
 		br.close();
 		urlConn.disconnect();
-		
+
 		System.out.println(responseText);
 		return responseText;
 	}
+
+	// xml 형식으로 대기오염 OpenApi 활용하기
+	// @RequestMapping(value="air", produces = "text/xml; charset=UTF-8")
+	@ResponseBody
+	public String airPollution(String location) throws IOException {
+
+		// OpenAPI 서버로 요청하고자 하는 url 작성
+		String url = "http://apis.data.go.kr/B552584/ArpltnInforInqireSvc/getCtprvnRltmMesureDnsty";
+		url +="?serviceKey=" + SERVICEKEY;
+		url +="&sidoName=" + URLEncoder.encode(location,"UTF-8");
+		url +="&returnType=xml";
+		url +="&numOfRows=10"; // 결과 개수
+
+		// 1. 작성된 url 정보를 넣어 URL 객체 생성
+		URL requestUrl = new URL(url);
+
+		// 2. 생성된 URL 객체로 URLConnection 생성
+		HttpURLConnection urlConn = (HttpURLConnection) requestUrl.openConnection();
+
+		// 3. 요청 시 필요한 Header 설정
+		urlConn.setRequestMethod("GET");
+
+		// 4. 해당 OpenAPI 서버로 요청 후 입력스트림을 통해서 응답데이터 읽어오기
+		BufferedReader br = new BufferedReader(new InputStreamReader(urlConn.getInputStream()));
+
+		String responseText = "";
+		String line;
+
+		while((line = br.readLine()) != null){
+			responseText += line;
+		}
+
+		// 5. 다 사용한 스트링 반납 및 연결 해제
+		br.close();
+		urlConn.disconnect();
+
+		System.out.println(responseText);
+		
+		return responseText;
+
+	}
+	
+	// xml 형식으로 지진해일 대피소 OpenApi 활용하기
+	@RequestMapping(value="shelter",produces = "text/xml; charset=UTF-8")
+	@ResponseBody
+	public String shelterList() throws IOException{
+		
+		// OpenAPI 서버로 요청하고자 하는 url 작성
+				String url = "http://apis.data.go.kr/1741000/TsunamiShelter3/getTsunamiShelter1List";
+				url +="?serviceKey=" + SERVICEKEY;
+				url +="&pageNo=1";
+				url +="&numOfRows=20"; // 결과 개수
+				url +="&returnType=xml";
+
+				// 1. 작성된 url 정보를 넣어 URL 객체 생성
+				URL requestUrl = new URL(url);
+				
+				System.out.println(url);
+				// 2. 생성된 URL 객체로 URLConnection 생성
+				HttpURLConnection urlConn = (HttpURLConnection) requestUrl.openConnection();
+
+				// 3. 요청 시 필요한 Header 설정
+				urlConn.setRequestMethod("GET");
+
+				// 4. 해당 OpenAPI 서버로 요청 후 입력스트림을 통해서 응답데이터 읽어오기
+				BufferedReader br = new BufferedReader(new InputStreamReader(urlConn.getInputStream()));
+
+				String responseText = "";
+				String line;
+
+				while((line = br.readLine()) != null){
+					responseText += line;
+				}
+
+				// 5. 다 사용한 스트링 반납 및 연결 해제
+				br.close();
+				urlConn.disconnect();
+
+				System.out.println(responseText);
+				
+				return responseText;
+		
+	}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
--- 6_Framework/openDataProject/src/main/webapp/index.jsp
+++ 6_Framework/openDataProject/src/main/webapp/index.jsp
@@ -37,17 +37,41 @@
       <tbody></tbody>
    </table>
    
+   <hr>
+   
+   <h1>실시간 지진해일 긴급 대피장소</h1>
+   
+   <button id="btn2">실시간 지진해일 긴급 대피장소 정보</button>
+   <br><br>
+   
+   <table border="1" id="result2">
+      <thead>
+         <tr>
+            <th>시도명</th>
+            <th>시군구명</th>
+            <th>대피지구명</th>
+            <th>대피장소명</th>
+            <th>주소</th>
+            <th>경도</th>
+            <th>위도</th>
+            <th>수용가능인원수</th>
+            <th>대피소 분류명</th>
+         </tr>
+      </thead>
+      <tbody></tbody>
+   </table>
+   
    <script>
    
    $(function(){
 	   $("#btn1").click(function(){
-			
+		  //Json 형식으로 응답 받을 때
 		   $.ajax({
 				url : "air",
 				data : { location : $("#location").val()},
 				success : function(data) {
-					// console.log(data);
-					// console.log(data.response.body.items);
+					 console.log(data);
+					 //console.log(data.response.body.items);
 					
 					const itemArr = data.response.body.items;
 					
@@ -74,17 +98,114 @@
 				}
 			   
 			   
-			   
-			   
 		   })
 		   
+		   // ---------------------------------------------------------------
+		   // 응답 데이터를 xml 형식으로 받을 때
+		   $.ajax({
+			   url : "air",
+			   data : {location : $("#location").val()},
+			   success : function(result){
+				   console.log(result);
+				   
+				   // $('요소명').find(매개변수)
+				   // - 기준이 되는 요소의 하위 요소들 중 특정 요소를 찾을 때 사용
+				   // - html, xml은 같은 markup language이기 때문에 사용 가능하다.
+				   // console.log($(result).find("item"));
+				   
+				   // xml형식의 응답데이터를 받았을 때
+				   // 1. 넘겨받은 데이터를 $() 제이쿼리화 시킨후
+				   //    응답데이터 안에 실제 데이터가 담겨 있는 요소 선택
+				   const itemArr = $(result).find("item");
+				   
+				   // 2. 반복문을 통해 실제 데이터가 담긴 요소들에 접근해서 동적으로 요소 만들기
+				   let value;
+				   itemArr.each(function(index, item){
+					   
+					   // console.log(item);
+					   // console.log($(item).find("stationName").text());
+					   
+					   value += "<tr>"
+							+"<td>"+ $(item).find("stationName").text() +"</td>"
+							+"<td>"+ $(item).find("dataTime").text() +"</td>"
+							+"<td>"+ $(item).find("khaiValue").text() +"</td>"
+							+"<td>"+ $(item).find("pm10Value").text() +"</td>"
+							+"<td>"+ $(item).find("so2Value").text() +"</td>"
+							+"<td>"+ $(item).find("coValue").text() +"</td>"
+							+"<td>"+ $(item).find("no2Value").text() +"</td>"
+							+"<td>"+ $(item).find("o3Value").text() +"</td>"
+						+"</tr>"
+					   
+				   })
+				   
+				   // 3. 동적으로 만들어낸 요소를 화면에 출력
+				   $("#result1 > tbody").html(value);
+			   },
+			   
+			   error : function(){
+				   console.log("통신 실패");
+			   }
+			   
 		   
+		   
+		   })
+		   
+					   
 	   })
-	   
-	   
    })
    
-   
+   $(function(){
+	
+	   $("#btn2").click(function(){
+		   
+		// 응답 데이터를 xml 형식으로 받을 때
+		   $.ajax({
+			   url : "shelter",
+			   success : function(result){
+				   
+				   // $('요소명').find(매개변수)
+				   // - 기준이 되는 요소의 하위 요소들 중 특정 요소를 찾을 때 사용
+				   // - html, xml은 같은 markup language이기 때문에 사용 가능하다.
+				   // console.log($(result).find("item"));
+				   
+				   // xml형식의 응답데이터를 받았을 때
+				   // 1. 넘겨받은 데이터를 $() 제이쿼리화 시킨후
+				   //    응답데이터 안에 실제 데이터가 담겨 있는 요소 선택
+				   const itemArr = $(result).find("row");
+				   
+				   // 2. 반복문을 통해 실제 데이터가 담긴 요소들에 접근해서 동적으로 요소 만들기
+				   let value;
+				   itemArr.each(function(index, item){
+					   
+					   // console.log(item);
+					   // console.log($(item).find("stationName").text());
+					   
+					   value += "<tr>"
+							+"<td>"+ $(item).find("sido_name").text() +"</td>"
+							+"<td>"+ $(item).find("sigungu_name").text() +"</td>"
+							+"<td>"+ $(item).find("remarks").text() +"</td>"
+							+"<td>"+ $(item).find("shel_nm").text() +"</td>"
+							+"<td>"+ $(item).find("address").text() +"</td>"
+							+"<td>"+ $(item).find("lon").text() +"</td>"
+							+"<td>"+ $(item).find("lat").text() +"</td>"
+							+"<td>"+ $(item).find("shel_av").text() +"</td>"
+							+"<td>"+ $(item).find("shel_div_type").text() +"</td>"
+						+"</tr>"
+					   
+				   })
+				   
+				   // 3. 동적으로 만들어낸 요소를 화면에 출력
+				   $("#result2 > tbody").html(value);
+			   },
+			   
+			   error : function(){
+				   console.log("통신 실패");
+			   }
+		   
+		   })
+		   
+	   })
+   })
    
    
    </script>
